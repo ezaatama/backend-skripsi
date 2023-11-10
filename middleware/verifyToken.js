fe6c66a19646d03d28dbdf45b39f618b00d78dc8
ezaatama <ezatama33@gmail.com>
2023-11-10 13:33:47 +0700

    update response status

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,7 +5,7 @@ const verifyToken = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Sesi Anda Telah Habis!" });
+    if (err) return res.status(401).json({ message: "Sesi Anda Telah Habis!" });
     req.email = decoded.email;
     req.user = decoded.userId;
 
