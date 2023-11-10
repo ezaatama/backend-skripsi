39d889ddbb4e0bc9fdcc35d85e9cafcf421171f3
ezaatama <ezatama33@gmail.com>
2023-11-10 11:20:26 +0700

    middleware me

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { me, login, logout } = require("../controllers/auth");
+const middlewareToken = require("../middleware/verifyToken");
 const router = express.Router();
 
-router.get("/me", me);
+router.get("/me", middlewareToken, me);
 router.post("/login", login);
 router.post("/logout", logout);
 
