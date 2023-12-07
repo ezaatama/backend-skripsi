1b9540d324d1c4825bf228386b71e4be49b3ae26
ezaatama <ezatama33@gmail.com>
2023-12-07 10:50:58 +0700

    lint database

--- config/databases.js
+++ config/databases.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('sandboxg_db_homers', 'sandboxg_ezaatama', 'rezaputra13', {
+const sequelize = new Sequelize(
+  "sandboxg_db_homers",
+  "sandboxg_ezaatama",
+  "rezaputra13",
+  {
     host: "api.sandbox-gethome.my.id",
-    dialect: "mysql"
-})
+    dialect: "mysql",
+  }
+);
 
 // const sequelize = new Sequelize('skripsweet_homers', 'root', '', {
 //     host: "localhost",
