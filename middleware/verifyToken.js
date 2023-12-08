f97d984d45169859f3960c62da8ab4bab157eb84
ezaatama <ezatama33@gmail.com>
2023-12-08 10:00:02 +0700

    update message unauthorized

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
+  if (token == null) return res.status(401).json({ message: "Unauthorized" });
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.status(401).json({ message: "Sesi Anda Telah Habis!" });
     req.email = decoded.email;
