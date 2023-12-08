47ebd12cc36da54fa6b57d47a624e45e4656c5ec
ezaatama <ezatama33@gmail.com>
2023-12-08 10:00:12 +0700

    new routes login mobile

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { me, login, logout } = require("../controllers/auth");
+const { me, login, loginM, logout } = require("../controllers/auth");
 const middlewareToken = require("../middleware/verifyToken");
 const router = express.Router();
 
 router.get("/me", middlewareToken, me);
 router.post("/login", login);
+router.post("/loginM", loginM);
 router.post("/logout", logout);
 
 module.exports = router;
