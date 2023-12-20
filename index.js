ec387fa517d7f9c005abedb6bbaa1c9fc8c30fa8
ezaatama <ezatama33@gmail.com>
2023-12-20 13:22:20 +0700

    try fixing cors

--- index.js
+++ index.js
@@ -54,15 +54,23 @@ app.use(
   })
 );
 
-const corsOptions = {
-  exposedHeaders: ["Authorization", "x-access-token"],
-  credentials: true,
-  origin: ["https://sandbox-gethome.my.id", "http://localhost:3000"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   exposedHeaders: ["Authorization", "x-access-token"],
+//   credentials: true,
+//   origin: ["https://sandbox-gethome.my.id", "http://localhost:3000"],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   optionsSuccessStatus: 204,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization, x-access-token','http://localhost:3000');
+  if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
